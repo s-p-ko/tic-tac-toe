a0084a4505d9e3f661d4295e2f17b8d964a26f6f
Serhii Kotsiuba <s-p-ko@ukr.net>
2021-05-23 13:07:53 +0300

    Update Board & utils

---
+++ src/Game/components/Board.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './../../Game/Game.css'
+
+const Board = () => {
+
+
+
+    return (
+        <div>
+            Board here
+        </div>
+    )
+}
+
+export default Board
---
+++ src/Game/components/utils.js
@@ -0,0 +1,13 @@
+export const calculateStatus = () => {
+
+}
+
+export const calculateNextValue = () => {
+
+    // return  'X' or 'O'
+}
+
+export const calculateWinner = () => {
+
+    // return winner or  null
+}
