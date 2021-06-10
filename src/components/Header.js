f9dc65ce1065551a918352f514830ff1034ebb2a
Serhii Kotsiuba <s-p-ko@ukr.net>
2021-06-10 07:12:25 +0300

    Update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import React from 'react'
 
-
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
 import logo from './../logo.svg'
 
 const Header = () => {
     return (
-        <nav>
-            <div>
-                <a href="https://github.com/s-p-ko/" target="_blank" rel="noreferrer">GitHub repository</a>
-                <div>Tic Tac Toe</div>
-                <a href="https://reactjs.org/" target="_blank" rel="noreferrer"><img src={logo} className="d-inline-block align-text-top" width="50" height="50" alt="logo" /></a>
-            </div>
-        </nav>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="https://github.com/s-p-ko/tic-tac-toe" target="_blank" rel="noreferrer">GitHub repository</Navbar.Brand>
+                <Navbar.Brand>Tic Tac Toe</Navbar.Brand>
+                <Navbar.Brand href="https://reactjs.org/" target="_blank" rel="noreferrer"><img src={logo} className="d-inline-block align-text-top" width="50" height="50" alt="logo" /></Navbar.Brand>
+            </Container>
+        </Navbar>
     )
 }
 
