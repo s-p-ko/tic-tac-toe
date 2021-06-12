6bcefc3a071fad4d7e0d882f67b3a6ebe845797c
Serhii Kotsiuba <s-p-ko@ukr.net>
2021-06-12 15:44:02 +0300

    Update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,28 @@
 import React from 'react'
 
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const Footer = () => {
     const year = new Date().getFullYear()
     return (
-        <footer className=""
-            style={{}}
+        <footer className="d-none d-lg-block d-md-block"
+            style={{
+                backgroundColor: "#f8f9fa",
+                marginTop: "3rem",
+                padding: "1rem 0",
+                position: "absolute",
+                bottom: "0px",
+                width: "100%"
+            }}
         >
-            <div>
-                <div>
-                    <div><a href="https://github.com/s-p-ko/"><b>s-p-ko</b></a></div>
-                    <div>Logo© {year}</div>
-                </div>
-            </div>
+            <Container>
+                <Row>
+                    <Col md={6} className="text-start"><a href="https://github.com/s-p-ko/tic-tac-toe"><b>s-p-ko</b></a></Col>
+                    <Col md={6} className="text-end">Logo© {year}</Col>
+                </Row>
+            </Container>
         </footer>
     )
 }
