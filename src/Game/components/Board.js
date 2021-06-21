fb3fe9e3d00e520b9a27bb01c962f8b96e87531d
Serhii Kotsiuba <s-p-ko@ukr.net>
2021-06-21 23:26:47 +0300

    Update Board & add styles

--- src/Game/components/Board.js
+++ src/Game/components/Board.js
@@ -2,13 +2,32 @@ import React from 'react'
 
 import './../../Game/Game.css'
 
-const Board = () => {
-
-
+const Board = ({ squares, onClick }) => {
+    function renderSquare(i) {
+        return (
+            <button className="square" onClick={() => onClick(i)}>
+                {squares[i]}
+            </button>
+        )
+    }
 
     return (
         <div>
-            Board here
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
         </div>
     )
 }
